66b35a8171d1a265dbe8b9f4f9f2c8050822b82d
Merlin33069 <Merlin33069@gmail.com>
2012-09-04 04:18:55 -0400

    Updated something small >_>

--- MCFrog/Configuration.cs
+++ MCFrog/Configuration.cs
@@ -6,7 +6,7 @@
 		internal static byte MAXPLAYERS = 100;
 		internal static bool PUBLIC = true;
 		internal static byte VERSION = 7;
-		internal static string ServerName = "[MCFrog] Alpha Server";
+		internal static string ServerName = "[MineFrog] Alpha Server";
 		internal static string ServerMotd = "Alpha MCFrog Server +hax";
 		internal static string ServerSalt = "th1515A5al7";
 	}
